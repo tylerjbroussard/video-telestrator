0ba5071536eb4a799d64bafd429acfcc283c5def
tylerjbroussard <tylerjbroussard@live.com>
2018-06-03 00:01:28 -0700

    20180603 add canvas

---
+++ Telestrator UWP/js/draw.js
@@ -0,0 +1,69 @@
+﻿var canvas, stage;
+var drawingCanvas;
+var oldPt;
+var oldMidPt;
+var title;
+var color;
+var stroke;
+var colors;
+var index;
+
+function init() {
+    canvas = document.getElementById("myCanvas");
+    index = 0;
+    colors = ["yellow"];
+
+    //check to see if we are running in a browser with touch support
+    stage = new createjs.Stage(canvas);
+    stage.autoClear = false;
+    stage.enableDOMEvents(true);
+
+    createjs.Touch.enable(stage);
+    createjs.Ticker.framerate = 24;
+
+    drawingCanvas = new createjs.Shape();
+
+    stage.addEventListener("stagemousedown", handleMouseDown);
+    stage.addEventListener("stagemouseup", handleMouseUp);
+
+    title = new createjs.Text("Click and Drag to draw", "36px Arial", "yellow");
+    title.x = 300;
+    title.y = 200;
+    stage.addChild(title);
+
+    stage.addChild(drawingCanvas);
+    stage.update();
+}
+
+function handleMouseDown(event) {
+    if (!event.primary) { return; }
+    if (stage.contains(title)) {
+        stage.clear();
+        stage.removeChild(title);
+    }
+    color = colors[(index++) % colors.length];
+    stroke = 5;
+    oldPt = new createjs.Point(stage.mouseX, stage.mouseY);
+    oldMidPt = oldPt.clone();
+    stage.addEventListener("stagemousemove", handleMouseMove);
+}
+
+function handleMouseMove(event) {
+    if (!event.primary) { return; }
+    var midPt = new createjs.Point(oldPt.x + stage.mouseX >> 1, oldPt.y + stage.mouseY >> 1);
+
+    drawingCanvas.graphics.clear().setStrokeStyle(stroke, 'round', 'round').beginStroke(color).moveTo(midPt.x, midPt.y).curveTo(oldPt.x, oldPt.y, oldMidPt.x, oldMidPt.y);
+
+    oldPt.x = stage.mouseX;
+    oldPt.y = stage.mouseY;
+
+    oldMidPt.x = midPt.x;
+    oldMidPt.y = midPt.y;
+
+    stage.update();
+}
+
+function handleMouseUp(event) {
+    if (!event.primary) { return; }
+    stage.removeEventListener("stagemousemove", handleMouseMove);
+}
