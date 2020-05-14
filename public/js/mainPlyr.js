182fc3970148d6b59ebf12bd2d568fe2a8c350e6
unknown <tylerjbroussard@live.com>
2020-05-14 16:45:11 -0700

    addVideo method push

--- public/js/mainPlyr.js
+++ public/js/mainPlyr.js
@@ -2,17 +2,18 @@
 // https://github.com/sampotts/plyr/#options
 const videoDiv = $('#videoSpace');
 
-// Expose player so it can be used from the console
 
 
 
 var addVideo = function(vendor, videoId){
-    videoDiv.html(`<h1>Test</h1><div id="player" data-plyr-provider="${vendor}" data-plyr-embed-id="${videoId}">`);
+
+    videoDiv.html(`<h1>Test</h1><div id="player" data-plyr-provider="${vendor}" data-plyr-embed-id="${videoId}">`);    
+
+    //const player has to be redeclared 
     const player = new Plyr('#player');
+    
+    // Expose player so it can be used from the console
     window.player = player;
 };
 
 addVideo('youtube', 'nIkHXY1QbVE');
-
-
-//const player has to be redeclared 
