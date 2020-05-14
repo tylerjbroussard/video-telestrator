8d6066ae6c09dadee4c7c9e6b79c854a6d72f76b
unknown <tylerjbroussard@live.com>
2020-05-14 16:43:43 -0700

    working addVideo method

--- public/js/mainPlyr.js
+++ public/js/mainPlyr.js
@@ -1,6 +1,18 @@
 ﻿// Change "{}" to your options:
 // https://github.com/sampotts/plyr/#options
-const player = new Plyr('#player');
+const videoDiv = $('#videoSpace');
 
 // Expose player so it can be used from the console
-window.player = player;
+
+
+
+var addVideo = function(vendor, videoId){
+    videoDiv.html(`<h1>Test</h1><div id="player" data-plyr-provider="${vendor}" data-plyr-embed-id="${videoId}">`);
+    const player = new Plyr('#player');
+    window.player = player;
+};
+
+addVideo('youtube', 'nIkHXY1QbVE');
+
+
+//const player has to be redeclared 
